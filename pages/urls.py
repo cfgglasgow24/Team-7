34b7b183b5c1d78a2ea7c5581d70b79c59fc4cd5
PedroGGBM <pgrondagarrigues@gmail.com>
2024-04-11 14:47:23 +0100

    Added filter and request visualization

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,6 @@ from pages import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("filter/", views.filter, name='filter'),
+    path("request/", views.request, name='request'),
 ]
