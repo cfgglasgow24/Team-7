682c373e75b1a31630e6c06460ffc41ba9f4518b
Connor <connor.peter.donnelly@gmail.com>
2024-04-11 15:20:40 +0100

    urls and views added

--- pages/views.py
+++ pages/views.py
@@ -9,3 +9,33 @@ def filter(request):
 
 def request(request):
     return render(request, 'request.html', {})
+
+def cv(request):
+    return render(request, 'cv.html', {})
+
+def filter(request):
+    return render(request, 'filter.html', {})
+
+def job_opportunitites(request):
+    return render(request, 'job_opportunities.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+def mentee_view(request):
+    return render(request, 'mentee_view.html', {})
+
+def mentor_view(request):
+    return render(request, 'mentor_view.html', {})
+
+def mentorship(request):
+    return render(request, 'mentorship.html', {})
+
+def register_mentee(request):
+    return render(request, 'register_mentee.html', {})
+
+def register_mentor(request):
+    return render(request, 'register_mentor.html', {})
+
+def support(request):
+    return render(request, 'support.html', {})
