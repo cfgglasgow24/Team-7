fb6e53ebe871f85a287495777d1c115954db3ff8
xd22898 <xd22898@bristol.ac.uk>
2024-04-11 14:34:02 +0100

    fixing links

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,13 @@
 from django.urls import path
 from pages import views
 
+app_name = 'CodeDivisionMentoring'
+
 urlpatterns = [
     path("", views.home, name='home'),
+    path("", views.addEvent, name='addEvent'),
+    path("", views.cv, name='cv'),
+    path("", views.job_opportunities, name='job_opportunities'),
+    path("", views.support, name='support'),
+    path("", views.mentorship, name='mentorship'),   
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def addEvent(request):
+    return render(request, 'addEvent.html', {})
+
+def cv(request):
+    return render(request, 'cv.html', {})
+
+def job_opportunities(request):
+    return render(request, 'job_opportunities.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+def support (request):
+    return render(request, 'support.html', {})
+
+def mentorship (request):
+    return render (mentorship, 'mentorship.html', {})
